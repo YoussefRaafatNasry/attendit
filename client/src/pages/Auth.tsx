613f95b109d01e25c45f653977207ed2d2a11bdd
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2020-02-14 01:36:13 +0200

    [CLIENT] Create Auth Form UI

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -2,6 +2,31 @@ import React, { Component } from "react";
 
 export default class AuthPage extends Component {
   render() {
-    return <h1>Auth</h1>;
+    return (
+      <form className="auth-form">
+        <h2>attendit</h2>
+        <p className="auth-form__header">
+          Attendit let you explore and book events near you. Start now by
+          creating an account or login if you already have one.
+        </p>
+        <input
+          className="auth-form__input"
+          id="email"
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          className="auth-form__input"
+          id="password"
+          type="password"
+          placeholder="Password"
+        />
+
+        <div className="auth-form__buttons">
+          <button>Login</button>
+          <button>Register</button>
+        </div>
+      </form>
+    );
   }
 }
